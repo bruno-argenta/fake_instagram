33651560604f3d6998148b931b09e567fdc74cd9
faulestrade <faustiles99@gmail.com>
2024-10-23 20:45:51 -0300

    get all users

--- api-node/controllers/userController.js
+++ api-node/controllers/userController.js
@@ -1,21 +1,32 @@
-const User = require('../models/User');
-
+const User = require("../models/User");
 
 const getUserProfile = async (req, res) => {
-    try {
-        
-        const user = await User.findById(req.params.id).select('-password'); // Excluir la contraseña
-        if (!user) {
-            return res.status(404).json({ message: 'Usuario no encontrado' });
-        }
-        // Devolver los datos del usuario
-        res.status(200).json(user);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error del servidor' });
+  try {
+    const user = await User.findById(req.params.id).select("-password"); // Excluir la contraseña
+    if (!user) {
+      return res.status(404).json({ message: "Usuario no encontrado" });
     }
+    // Devolver los datos del usuario
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error del servidor" });
+  }
+};
+
+const getAllUsers = async (req, res) => {
+  try {
+    // Obtener todos los usuarios sin mostrar las contraseñas
+    const users = await User.find().select("-password");
+    res.status(200).json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error del servidor" });
+  }
 };
 
 module.exports = {
-    getUserProfile,
+  getUserProfile,
+  getAllUsers,
 };
+
